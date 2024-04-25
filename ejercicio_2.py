c0c6b6fb92e092bcadb426b5e7b1a33e5eac2ee4
Camila10921 <167362937+Camila10921@users.noreply.github.com>
2024-04-25 06:41:34 -0500

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,7 @@
+#variables
+base=int(input("medida de la base del rectangulo: ")) #declaramos la variable en donde se va a registrar la medida de la base del rectangulo
+altura=int(input("medida de la altura del rectangulo: ")) #declaramos la variable en donde se va a registrar la medida de la altura del rectangulo
+perimetro=base*2 + altura*2 #declaramos la variable en donde nos va a realizar el ejercicio del perimetro del rectangulo
+area=base*altura #declaramos la variable en donde nos va a realizaar el area del rectangulo
+
+print("el perimetro del rectangulo es",perimetro, "el area del rectangulo es", area) #colocamos las variables que se definieron con su orden para que se realice la operación y asi mismo se cree la frase 
